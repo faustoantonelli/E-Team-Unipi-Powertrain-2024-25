b1680e1cd0668d340395ed1b27624d73f010e970
Fausto Antonelli <f.antonelli11@studenti.unipi.it>
2026-01-01 19:11:15 +0100

    mod a vSpeed

--- Traction Control 2025-26/Classic Control/VehicleSpeed.cpp
+++ Traction Control 2025-26/Classic Control/VehicleSpeed.cpp
@@ -1,6 +1,8 @@
 #include "VehicleSpeed.h"
 
+// Questa funzione verrà "chiamata" dal main che sta in un altro file
 float VehicleSpeed(float FLSpeed, float FRSpeed, float Ax, float Ay, float Az) {
-    float Speed = 100.0f;
-    return Speed;
+    // Diciamo al compilatore di ignorare che non usiamo questi dati
+    (void)FLSpeed; (void)FRSpeed; (void)Ax; (void)Ay; (void)Az;
+    return 100.0f; // Restituisce sempre 100
 }
